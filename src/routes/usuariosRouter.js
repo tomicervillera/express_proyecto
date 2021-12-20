4225362a30b09fa605e568ffde0c1f1981afbfc4
tomicervillera <cervilleratomas@outlook.com>
2021-12-20 13:25:23 -0300

    Agregado paquete de sesión y corregido import de usuarioController

--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const UsuariosController = require("../controllers/UsuariosController");
+const UsuariosController = require("../controllers/usuariosController");
 
 router.post("/", UsuariosController.getUsuarioByEmailPass);
 
