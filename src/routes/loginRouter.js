f9274fd959dda38145080b0c35b9a758a2e8fdb0
tomicervillera <cervilleratomas@outlook.com>
2021-12-20 13:49:17 -0300

    fix: error de tipeo

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const loginController = require("../controllers/LoginController");
+const loginController = require("../controllers/loginController");
 
 router.post("/", loginController.login);
 
