5cae2cabd10dcd3e0349db2abeef867092878aa4
tomicervillera <cervilleratomas@outlook.com>
2021-12-20 16:21:47 -0300

    intento de login v2

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,13 +1,36 @@
-//const express = require("express");
-const usuariosController = require('./usuariosController');
-//const mongoose = require('mongoose');
+const models = require('../models/index');
 
-//const objectIdValidator = mongoose.Types.ObjectId;
 
 
 const login = async (req, res) => {
-    usuariosController.getUsuarioByEmailPass(req, res);
+    try {
+        
+        console.log(req.session.usuarioActual);
+        const nombreUsuario = req.body.nombreUsuario;
+        const contraseña = req.body.contraseña;
+
+        if (!nombreUsuario) {
+            return res.status(400).json({ msg: "nombreUsuario field is required.", error: true });
+        }
+        if (!contraseña) {
+            return res.status(400).json({ msg: "contraseña field is required.", error: true });
+        }
+        const response = await models.Usuarios.findOne({ nombreUsuario: req.body.nombreUsuario, contraseña: req.body.contraseña });
+        if (response) {
+            req.session.usuarioActual = response.nombreUsuario;
+            req.session.permisos = response.permisos;
+            req.session.save();
+            //console.log(req.session);
+            let data = { nombreUsuario: response.nombreUsuario, permisos: response.permisos };
+            return res.status(200).json({ data, error: false });
+        }
+        else {
+            return res.status(404).json({ msg: `User and/or password not valid.`, error: true });
+        }
+    } catch (error) {
+        return res.status(500).json({ msg: error, error: true });
+    }
 }
 
 
-module.exports = { login: login};
+module.exports = { login: login };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require ("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -13,6 +14,7 @@ const router = require("./routes/index");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(session({secret: '123456', resave: true, saveUninitialized: true}));
 app.use(router);
 
 const connectDb = async () => {
